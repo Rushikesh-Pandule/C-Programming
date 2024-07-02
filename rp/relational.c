0cd16d89d0e276c43a13ce018b1b33d9e3e9c962
Rushikesh-Pandule <rushipandule50@gmail.com>
2024-07-02 18:39:30 +0530

    all relational operators

---
+++ rp/relational.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,c,d,e,f,g,h;
+    a=13;
+    b=14;
+    c=a>b;
+    d=a<b;
+    e=a>=b;
+    f=a<=b;
+    g=a==b;
+    h=a!=b;
+    printf("\n a greater than b %d:",c);
+    printf("\n a less than b %d:",d);
+    printf("\n a greater than equal to b %d:",e);
+    printf("\n a less than equal to b %d:",f);
+    printf("\n a equal to equal to b %d:",g);
+    printf("\n a not equal to b is %d:",g);
+
+}
