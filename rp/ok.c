dae0a5094fbe6bf08187f0f0da738a892987fb51
Rushikesh-Pandule <rushipandule50@gmail.com>
2024-07-05 19:18:31 +0530

    c

---
+++ rp/ok.c
@@ -0,0 +1,16 @@
+/* takes nothing returns nothing*/
+
+#include <stdio.h>
+
+int main()
+{
+    add()
+}
+    add()
+{
+        int a,b,c;
+        printf("\n enter numbers");
+        scanf("\n %d",&a,&b);
+        c=a+b;
+        printf("\n sum is = %d",c);
+    }
